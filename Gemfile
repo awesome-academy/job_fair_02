9632cc3cc0ba4cbfebbe31026cbeb9570e1e1f99
Bui Ngoc May <ngocmay1597@gmail.com>
2018-11-09 13:36:55 +0700

    Ref task #3391 Create CV

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 ruby "2.5.1"
 
 gem "bootsnap", ">= 1.1.0", require: false
+gem "cocoon"
 gem "coffee-rails", "~> 4.2"
 gem "config"
 gem "devise"
--- app/models/curriculum_vitae.rb
+++ app/models/curriculum_vitae.rb
@@ -1,11 +1,15 @@
 class CurriculumVitae < ApplicationRecord
   belongs_to :user
+
   has_many :curriculum_vitae_details, dependent: :destroy
   has_many :curriculum_vitae_jobs, dependent: :destroy
   has_many :jobs, through: :curriculum_vitae_jobs,
     dependent: :destroy
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
+
+  accepts_nested_attributes_for :curriculum_vitae_details, allow_destroy: true,
+    reject_if: :all_blank
 
   validates :industry, presence: true,
     length: {maximum: Settings.max_length_title}
@@ -16,4 +20,8 @@ class CurriculumVitae < ApplicationRecord
   validates :skill, presence: true, length: {maximum: Settings.max_length_level}
   validates :language, presence: true,
     length: {maximum: Settings.max_length_lang}
+
+  CURRICULUM_VITAE_PARAMS = [:industry, :target, :experience, :skill, :language,
+    :public, curriculum_vitae_details_attributes:
+      %i(id title content _destroy)].freeze
 end
